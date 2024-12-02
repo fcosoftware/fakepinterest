07e817bb395ba58a4ebd297247af515b84e5388f
fcosoftware <fcosoftware35@gmail.com>
2024-12-02 00:15:07 -0300

    Link do banco de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -7,11 +7,12 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-if os.getenv("DEBUG") == 0:
-    link_banco = os.getenv("DATABASE_URL")
-else:
-    link_banco = "sqlite:///comunidade.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
+#if os.getenv("DEBUG") == 0:
+ #   link_banco = os.getenv("DATABASE_URL")
+#else:
+ #   link_banco = "sqlite:///comunidade.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "1e410970e973a8ac5d8710d6d20403af"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
