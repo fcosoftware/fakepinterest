4ad0776a0f14049e65df9789d9581bbd5e7fbc46
fcosoftware <fcosoftware35@gmail.com>
2024-12-02 00:04:42 -0300

    Mudança do banco de dados para PostgreSQL

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -11,7 +11,7 @@ if os.getenv("DEBUG") == 0:
     link_banco = os.getenv("DATABASE_URL")
 else:
     link_banco = "sqlite:///comunidade.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = link_banco
 app.config["SECRET_KEY"] = "1e410970e973a8ac5d8710d6d20403af"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
