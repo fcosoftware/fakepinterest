2fac222e11addc59b238ec3becd282db6d2f6393
fcosoftware <fcosoftware35@gmail.com>
2024-12-01 22:52:05 -0300

    Criação do arquivo requirements.txt

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,4 +1,5 @@
 # Criação no site
+import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +7,10 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+if os.getenv("DEBUG") == 0:
+    link_banco = os.getenv("DATABASE_URL")
+else:
+    link_banco = "sqlite:///comunidade.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "1e410970e973a8ac5d8710d6d20403af"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
